04d95db4dd7be823baf6f719e906637812057e61
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-10-29 14:37:25 -0400

    add note for webpack dev server

--- learning-webpack-4/exercise-files/Ch03/03_03/start/webpack.config.js
+++ learning-webpack-4/exercise-files/Ch03/03_03/start/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
+    // webpack dev server
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         port: 9000
